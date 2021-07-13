517d7af3291357d83e63d73b50711e5f9a2e862b
kholood-ea <kholood_ea@hotmail.com>
2021-07-13 22:31:50 +0200

    remove workspace build related files

--- home_service_robot/add_markers/CMakeFiles/add_markers.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for add_markers.
-# This may be replaced when dependencies are built.
--- home_service_robot/add_markers/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/robond/Documents/catkin_ws/src/home_service_robot/add_markers/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
