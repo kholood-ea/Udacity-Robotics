874cd4aa05e100a4cafc656b476d5de722156e57
root <kholood_ea@hotmail.com>
2021-07-14 19:21:13 +0000

    Merge branch 'main' of https://github.com/kholood-ea/Udacity-Robotics into main

--- home_service_robot/scripts/pick_objects.sh
+++ home_service_robot/scripts/pick_objects.sh
@@ -5,7 +5,6 @@ sleep 5
 
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" & 
 sleep 5
-
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm -e "source devel/setup.bash; rosrun pick_objects pick_objects"
