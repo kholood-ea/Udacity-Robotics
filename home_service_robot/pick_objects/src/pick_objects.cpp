466876c71ff7f03f99dfd5267273a8d34b3a8c13
root <kholood_ea@hotmail.com>
2021-07-16 23:05:57 +0000

    pick objects goal updated

--- home_service_robot/pick_objects/src/pick_objects.cpp
+++ home_service_robot/pick_objects/src/pick_objects.cpp
@@ -5,9 +5,11 @@
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "simple_navigation_goals");
+ 
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -24,8 +26,9 @@ int main(int argc, char** argv){
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  goal.target_pose.pose.position.x = -1.0;
+    goal.target_pose.pose.position.y = 0.0;
+   goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal");
@@ -37,12 +40,14 @@ int main(int argc, char** argv){
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
     ROS_INFO("Hooray, the base moved 1 meter forward");
+
   else
     ROS_INFO("The base failed to move forward 1 meter for some reason");
-     ros::Duration(5.0).sleep();
+     ros::Duration(10).sleep();
 // sending snd goal
-  goal.target_pose.pose.position.x = 2.0;
-  goal.target_pose.pose.orientation.w = -1.0;
+  goal.target_pose.pose.position.x = -1.5;
+  goal.target_pose.pose.position.y = 0.0;
+  goal.target_pose.pose.orientation.w = 1.0;
   
   ROS_INFO("Sending 2nd goal");
   ac.sendGoal(goal);
@@ -53,6 +58,7 @@ int main(int argc, char** argv){
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
     ROS_INFO("Hooray, the base moved 2 meters");
+
   else
     ROS_INFO("The base failed to move forward 2 meter ");
      ros::Duration(5.0).sleep();
