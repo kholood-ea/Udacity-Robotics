597b19bb069bf971419e8eb3afffad92977f26ef
root <kholood_ea@hotmail.com>
2021-07-17 17:01:57 +0000

    edit test_slam sh with own world

--- home_service_robot/scripts/test_slam.sh
+++ home_service_robot/scripts/test_slam.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/home_service_robot/map/map_my_world/worlds/kholood.world
 
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" & 
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &  
 sleep 5
 
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
